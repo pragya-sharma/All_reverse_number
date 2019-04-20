c942bf5f46962a55158f57ddaa9b293479b780f6
pragya-sharma <45152090+pragya-sharma@users.noreply.github.com>
2019-04-20 21:47:59 +0530

    Add files via upload

---
+++ reverse.py
@@ -0,0 +1,7 @@
+val = int(input("how many times  are value reverse "))
+length = int(input("enter length of string"))
+for i in range(n):
+    string = input("enter value for reverse")
+    if len(s) == l:
+        d = s[-1: :-1]
+    print(d)
